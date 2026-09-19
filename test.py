86860e2c3ed32babb87818db1d4b9e81a2ebb074
Atul Vishwakarma <atulvish39@gmail.com>
2026-09-20 01:39:27 +0530

    mcp edits

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-from tools.flight_tool import search_flights
-from backend import run_travel_agent
+import asyncio
 
-user_input = input("Enter travel request: ")
+# from mcp_client_test import get_all_tools
+from mcp_client import get_all_tools
 
-response = run_travel_agent(user_input=user_input, thread_id="test_user")
-
-print("\nFinal Response:\n")
-print(response["answer"])
+if __name__ == "__main__":
+    asyncio.run(get_all_tools())
