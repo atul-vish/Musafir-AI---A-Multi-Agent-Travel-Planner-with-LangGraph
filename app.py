bf8a3c9209e7b8f35f922fdc2327c85aa5923327
Atul Vishwakarma <atulvish39@gmail.com>
2026-09-19 13:56:36 +0530

    frontend

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ async def favicon():
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8001, reload=True)
